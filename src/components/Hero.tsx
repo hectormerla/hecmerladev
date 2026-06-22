bc01448b8a52bdfaac5b393f92f6e87e0c696106
Hector Merla <hector.merla@gmail.com>
2026-06-22 16:48:36 -0600

    Update logo and add brand colors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,13 @@
 
 const Hero = () => {
-  return <section className="container flex items-center justify-center">
-    <div className="flex items-center justify-start gap-2 uppercase">
-      <div className="w-2 h-2 rounded-full bg-test!"></div>
-      <span>Available for new projects</span>
+  return <section className="container flex flex-col items-start">
+    <div className="flex items-center justify-start gap-2 p-2 border border-line rounded-full">
+      <div className="w-2 h-2 rounded-full bg-primary"></div>
+      <span className="text-sm text-dim uppercase">Available for new projects</span>
     </div>
+    <h1 className="text-[clamp(38px,5.6vw,68px)] font-bold text-center">
+      I build fast, thoughtful
+      <span>web experiences</span></h1>
   </section>
 }
 
