542862b51db5292bd99265f3db59a4cc170b370d
Hector Merla <hector.merla@gmail.com>
2026-06-22 17:02:12 -0600

    Implement prettier to the project

--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -1,8 +1,15 @@
-
-
-const HeaderLink = ({ children, href }: { children: React.ReactNode; href: string }) => {
+const HeaderLink = ({
+  children,
+  href,
+}: {
+  children: React.ReactNode
+  href: string
+}) => {
   return (
-    <a className="text-sm text-dim hover:bg-bg-hover py-2 px-3 rounded-md transition duration-300" href={href}>
+    <a
+      className="text-sm text-dim hover:bg-bg-hover py-2 px-3 rounded-md transition duration-300"
+      href={href}
+    >
       {children}
     </a>
   )
